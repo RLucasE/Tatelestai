1186d3552f0c5407a33a87216edeb1b2a22056fa
RLucasE <lucascabjnmro2@gmail.com>
2025-04-09 11:54:10 -0300

    Carpeta example-app ya no es un submodulo

---
+++ Backend/example-app/routes/api.php
@@ -0,0 +1,20 @@
+<?php
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+
+
+Route::get('/testeeess', function () {
+    return ['Users' => User::all()];
+});
+
+
+Route::get('/test', function () {
+    return ['Users' => User::all()];
+});
