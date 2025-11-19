f6f3846a92cb35ebadbc2274a46ead8b16b8c6c2
RLucas <lucascabjnmro2@gmail.com>
2025-11-19 19:02:59 -0300

    Feature: implement reporting functionality with modal and API endpoint for offers and establishments

--- Backend/example-app/database/factories/ProductOfferFactory.php
+++ Backend/example-app/database/factories/ProductOfferFactory.php
@@ -7,7 +7,7 @@ use App\Models\Product;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\product_offer>
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\ProductOffer>
  */
 class ProductOfferFactory extends Factory
 {
