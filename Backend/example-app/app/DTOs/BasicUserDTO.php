b679a13f89f246ca8d2e228e8c1b23b97cd4d5cf
RLucas <lucascabjnmro2@gmail.com>
2026-09-20 12:30:20 -0300

    Checking github if pull requests are mandatory 2.0

--- Backend/example-app/app/DTOs/BasicUserDTO.php
+++ Backend/example-app/app/DTOs/BasicUserDTO.php
@@ -13,7 +13,9 @@ class BasicUserDTO
         public readonly string $email,
         /** @var string[] */
         public readonly array $roles,
-    ) {}
+    ) {
+        
+    }
 
     public static function fromModel(User $user): self
     {
